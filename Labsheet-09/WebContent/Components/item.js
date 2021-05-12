4a6eb0d900eec77620372de666eaf0248561b83b
PasinduManeka <manekaherat815@gmail.com>
2021-05-12 08:32:12 +0530

    set the functions for validation and submit the form.

--- Labsheet-09/WebContent/Components/item.js
+++ Labsheet-09/WebContent/Components/item.js
@@ -1,1 +1,73 @@
-console.log("Hello World!")
+//console.log("Hello World! bye!")
+
+//hide the alerts
+$(document).ready(function(){ 
+	if ($("#alertSuccess").text().trim() == "") { 
+		$("#alertSuccess").hide(); 
+	} 
+	$("#alertDanger").hide(); }
+);
+
+//Form validation
+function validationItemForm(){
+	//code
+	if($("#itemCode").val().trim()==""){
+		return "insert the item code.";
+	}
+	
+	//name
+	if($("#itemName").val().trim() == ""){
+		return "Insert the item name";
+	}
+	
+	//price
+	var itemPrice = $("#itemPrice").val().trim();
+	if(!$.isNumeric(itemPrice)){
+		return "Insert a numeric value for item price";
+	}
+	
+	//description
+	if($("#itemDesc").val().trim() == ""){
+		return "Insert the item description";
+	}
+	
+	return true;
+}
+
+//Save
+$(document).on("click", "#btnSave", function(event){
+	//clear alerts
+	$("#alertSuccess").text("");
+	$("#alertSuccess").hide();
+	$("#alertDanger").text("");
+	$("#alertDanger").hide();
+	
+	//form validation
+	var status = validationItemForm();
+	if(status != true){
+		$("#alertDanger").text(status);
+		$("#alertDanger").show();
+	}
+	
+	//if valid
+	$("#formItem").submit();
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
